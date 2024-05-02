43ca46da0f966efb0ebd94fb1d57660a56d39a59
Djimenez <20200093@ricaldone.edu.sv>
2024-05-01 21:41:17 -0600

    Cambios de diseño

--- app/src/main/java/diego/jimenez/emulador_20200093_diego/ActivityLogin.kt
+++ app/src/main/java/diego/jimenez/emulador_20200093_diego/ActivityLogin.kt
@@ -1,14 +1,14 @@
 package diego.jimenez.emulador_20200093_diego
 
 import android.content.Intent
-import android.graphics.drawable.shapes.OvalShape
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-class Activity_Login : AppCompatActivity() {
+class ActivityLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,11 +22,14 @@ class Activity_Login : AppCompatActivity() {
 
         val btnLogin = findViewById<Button>(R.id.btnLogin)
 
+
         btnLogin.setOnClickListener {
 
-           val intent =  Intent(this, MainActivity::class.java)
+            val intent =  Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+
+
 
 
     }
